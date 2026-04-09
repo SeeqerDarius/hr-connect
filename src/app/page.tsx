1d4e6e1859aa4347a4d57a6e8433173d2e60e371
SeeqerDarius <andrewsasante79@icloud.com>
2026-04-09 21:37:37 +0000

    Final 2026 update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+
 import { motion } from 'framer-motion';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Autoplay } from 'swiper/modules';
